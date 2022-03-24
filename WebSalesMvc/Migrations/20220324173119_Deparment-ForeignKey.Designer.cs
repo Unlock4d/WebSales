fc0ce6d2664baf100c0b0c4704f49cae1a99f19a
Frederico <frederico.fernandes2001@gmail.com>
2022-03-24 17:34:34 +0000

    DepartmentId in Sellers

--- WebSalesMvc/Migrations/20220324173119_Deparment-ForeignKey.Designer.cs
+++ WebSalesMvc/Migrations/20220324173119_Deparment-ForeignKey.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebSalesMvc.Data;
 
 namespace WebSalesMvc.Migrations
 {
     [DbContext(typeof(WebSalesMvcContext))]
-    partial class WebSalesMvcContextModelSnapshot : ModelSnapshot
+    [Migration("20220324173119_Deparment-ForeignKey")]
+    partial class DeparmentForeignKey
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- WebSalesMvc/Services/SellerService.cs
+++ WebSalesMvc/Services/SellerService.cs
@@ -19,6 +19,7 @@ namespace WebSalesMvc.Services
         }
         public void Insert(Seller obj)
         {
+            obj.Department = _context.Department.First();
             _context.Add(obj);
             _context.SaveChanges();
         }
