91b1818bac5de86641bd0017b2c7a1e21f73ae8a
Frederico <frederico.fernandes2001@gmail.com>
2022-03-25 11:38:10 +0000

    Formatação no website

--- WebSalesMvc/Startup.cs
+++ WebSalesMvc/Startup.cs
@@ -11,6 +11,8 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using WebSalesMvc.Data;
 using WebSalesMvc.Services;
+using System.Globalization;
+using Microsoft.AspNetCore.Localization;
 
 namespace WebSalesMvc
 {
@@ -39,8 +41,18 @@ namespace WebSalesMvc
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env,SeendingService seendingService)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, SeendingService seendingService)
         {
+            var enUS = new CultureInfo("en-US");
+            var localizationOptions = new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture(enUS),
+                SupportedCultures = new List<CultureInfo> { enUS },
+                SupportedUICultures = new List<CultureInfo> { enUS },
+            };
+            app.UseRequestLocalization(localizationOptions);
+
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
