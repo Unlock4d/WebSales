ff98061ac3adbc85487e0b416b14a791d567eb8b
Frederico <frederico.fernandes2001@gmail.com>
2022-03-25 10:16:23 +0000

    Delete method to sellers

--- WebSalesMvc/Controllers/SellersController.cs
+++ WebSalesMvc/Controllers/SellersController.cs
@@ -13,7 +13,7 @@ namespace WebSalesMvc.Controllers
         public SellersController(SellerService sellerService, Departmentservice departmentservice)
         {
             _sellerService = sellerService;
-            _derpartmentService = departmentservice;    
+            _derpartmentService = departmentservice;
         }
 
         public IActionResult Index()
@@ -24,7 +24,7 @@ namespace WebSalesMvc.Controllers
         public IActionResult Create()
         {
             var departments = _derpartmentService.FindAll();
-            var viewModel = new SellerFormViewModel { Departments = departments };  
+            var viewModel = new SellerFormViewModel { Departments = departments };
             return View(viewModel);
         }
         [HttpPost]
@@ -32,6 +32,27 @@ namespace WebSalesMvc.Controllers
         public IActionResult Create(Seller seller)
         {
             _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
+        }
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var obj = _sellerService.FindById(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+
+            }
+            return View(obj);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            _sellerService.Remove(id);
             return RedirectToAction(nameof(Index));
         }
     }
